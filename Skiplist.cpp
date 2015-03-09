becd7cc8f7d7e350250770c07689eada2cd80ace
Brendan Wilson <brendan.x.wilson@gmail.com>
2015-03-08 19:18:58 -0500

    Got addition and search working
    Finished the addition and search functions

--- Skiplist.cpp
+++ Skiplist.cpp
@@ -1,16 +1,159 @@
-#include 'Skiplist.h'
+#include <vector>
+#include <random>
+#include <iostream> // for test_print, delete later
+#include <ctime>
+#include "Skiplist.h"
 
-// Node
-Node::Node(E *newElem, int newHeight)
+///////////////////////////////// Node
+template <typename E>
+Node<E>::Node(E newElem, int newHeight)
 {
     height = newHeight;
     element = newElem;
-    nextList = new *Node[height];
+    nextList = new Node<E>*[height];
 }
 
-Node::~Node()
+template <typename E>
+Node<E>::~Node()
 {
     delete[] nextList;
 }
 
-// Skiplist
+template <typename E>
+bool operator< (const Node<E> &lhs, const E &rhs)
+{
+    return lhs.element < rhs;
+}
+
+/////////////////////////////// Skiplist
+// Constructor/destructor
+template <typename E>
+Skiplist<E>::Skiplist()
+{
+    size = 0;
+    negativeInf.push_back(NULL);
+    distribution = std::geometric_distribution<int> (0.5);
+    generator.seed(time(NULL));
+}
+
+template <typename E>
+Skiplist<E>::~Skiplist()
+{
+    Node<E> *current = negativeInf[0];
+    Node<E> *next;
+    while (current)
+    {
+        next = current->nextList[0];
+        delete current;
+        current = next;
+    }
+}
+
+// Private functions
+template <typename E>
+std::vector<Node<E>**> Skiplist<E>::search(const E &target)
+{
+    int level = negativeInf.size() - 1;
+    std::vector<Node<E>**> links;
+    Node<E> **ppCurrent = &negativeInf[level];
+
+    while (level >= 0)
+    {   
+        //std::cout << "level=" << level << std::endl;
+        //std::cout << *ppCurrent << std::endl;
+        //std::cin.ignore();
+        if (*ppCurrent && (*ppCurrent)->element < target)
+        {
+            // Move rightward
+            ppCurrent = &(*ppCurrent)->nextList[level];
+        }
+        else
+        {
+            // Move downward
+            links.push_back(ppCurrent);
+            --level;
+            --ppCurrent;
+        }
+    }
+
+    return links; 
+}
+
+// Public functions
+template <typename E>
+void Skiplist<E>::add(const E &newElement)
+{
+    std::vector<Node<E>**> links = search(newElement);
+    int height = distribution(generator) + 1;
+    int index = 0;
+    Node<E> *pNewNode = new Node<E>(newElement, height);
+
+    while (links.size() > 0 && index < height)
+    {
+        Node<E> **ppPrevNode = links.back();
+        links.pop_back();
+        Node<E> *pNextNode = *ppPrevNode;
+        *ppPrevNode = pNewNode;
+        pNewNode->nextList[index] = pNextNode;
+        ++index;
+    }
+
+    while (index < height) // We need to raise total height of list
+    {
+        negativeInf.push_back(pNewNode);
+        pNewNode->nextList[index] = NULL;
+        ++index;
+    }
+
+    /*
+    std::cout << "-inf: ";
+    for (unsigned i = 0; i < negativeInf.size(); ++i)
+    {
+        std::cout << negativeInf[i] << ' ';
+    }
+    std::cout << std::endl;
+    Node<E> *current = negativeInf[0];
+    while (current)
+    {
+        std::cout << current << ": ";
+        for (int i = 0; i < current->height; ++i)
+        {
+            std::cout << current->nextList[i] << ' ';
+        }
+        std::cout << std::endl;
+        current = current->nextList[0];
+    }*/
+
+    ++size;
+}
+
+template <typename E>
+void Skiplist<E>::test_print()
+{
+    using namespace std;
+
+    cout << "-infinity\t";
+    for (unsigned int i = 0; i < negativeInf.size(); ++i)
+        cout << "- ";
+    cout << endl;
+
+    Node<E> *current = negativeInf[0];
+    while (current)
+    {
+        int i = 0;
+        cout << current->element << "\t\t";
+        for (; i < current->height; ++i)
+            cout << "- ";
+        for (; i < signed(negativeInf.size()); ++i)
+            cout << "| ";
+        cout << endl;
+        current = current->nextList[0];
+    }
+
+    cout << "infinity\t";
+    for (unsigned int i = 0; i < negativeInf.size(); ++i)
+        cout << "- ";
+    cout << endl;
+}
+
+template class Skiplist<int>;
--- Skiplist.h
+++ Skiplist.h
@@ -1,19 +1,26 @@
 #ifndef SKIPLIST_H
 #define SKIPLIST_H
 
+#include <vector>
+#include <random>
+
+template<typename E> class Node;
+template<typename E> bool operator< (const Node<E>&, const E&);
+template<typename E> class Skiplist;
+
 template <typename E>
 class Node
 {
 private:
-    E *element;
-    Node *nextList;
+    E element;
+    Node<E> **nextList;
     int height;
 
-    Node();
+    Node(E, int);
     ~Node();
 
-    friend class Skiplist;
-
+    friend class Skiplist<E>;
+    friend bool operator< <>(const Node<E>&, const E&);
 };
 
 template <typename E>
@@ -21,14 +28,21 @@ class Skiplist
 {
 private:
     int size;
-    std::vector<*Node<E>> negativeInf;
-    Node positiveInf;
+    std::vector<Node<E>*> negativeInf;
+    // Positive infinity = NULL
 
-    std::vector<**Node<E>> search(E*);
+    std::default_random_engine generator;
+    std::geometric_distribution<int> distribution;
+
+    std::vector<Node<E>**> search(const E&);
 
 public:
-    void add(E*);
-    E* remove(E*);
+    Skiplist();
+    ~Skiplist();
+    void add(const E&);
+    //void remove(const E&);
+    //bool is_in(const E&);
+    void test_print();
 };
 
 #endif // SKIPLIST_H
